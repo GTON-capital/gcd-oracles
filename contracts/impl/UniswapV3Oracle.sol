d65e130acb0a62838d3795e924ecd0294cf79b8e
Alex Telegin <telegin.alexander@gmail.com>
2022-07-15 15:27:53 +0300

    Adjust UniswapV3 oracle params for Ethereum

--- contracts/impl/UniswapV3Oracle.sol
+++ contracts/impl/UniswapV3Oracle.sol
@@ -22,10 +22,10 @@ contract UniswapV3OracleGCD is IOracleUsd {
   mapping (address => QuoteParams) public quoteParams;
 
   // GCD Protocol Vault parameters
-  // Ropsten: 0x634cd07fce65a2f2930b55c7b1b20a97196d362f
+  // Ropsten: 0x24643a31abb6F06C451D2be5ED01D2f95571d14a
   // Goerli: 0x634cd07fce65a2f2930b55c7b1b20a97196d362f
-  // Ethereum: 
-  IVaultParameters public constant vaultParameters = IVaultParameters(0x634Cd07fce65a2f2930B55c7b1b20a97196d362F);
+  // Ethereum: 0x1c2a972841A64872178fC510CeE5C87301d91160
+  IVaultParameters public constant vaultParameters = IVaultParameters(0x1c2a972841A64872178fC510CeE5C87301d91160);
 
   // Uniswap V3 factory - same on all chaons
   address public constant factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
@@ -34,7 +34,7 @@ contract UniswapV3OracleGCD is IOracleUsd {
   // Ropsten: 0xc778417E063141139Fce010982780140Aa0cD5Ab
   // Goerli: 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6
   // Ethereum: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
-  address public defaultQuoteAsset = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
+  address public defaultQuoteAsset = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
   // 0.3%
   uint24 public constant defaultPoolFee = 3000;
@@ -42,10 +42,10 @@ contract UniswapV3OracleGCD is IOracleUsd {
   uint32 public defaultTWAPPeriod = 30 minutes;
 
   // GCD Protocol oracle registry
-  // Ropsten: 0x85d7676ff4339C7e59eb7e90F160E909fc65d3bd
+  // Ropsten: 0x6B5701e3c6Ae063c6cBf23000E146DaaFA6db6cF
   // Goerli: 0xC0B881e21eE1B847A659206C0214E3357788E88E
-  // Ethereum: 
-  IOracleRegistry public constant oracleRegistry = IOracleRegistry(0xC0B881e21eE1B847A659206C0214E3357788E88E);
+  // Ethereum: 0x2E9B7ce6124284Fb6Afe402A619278E92Ac8Cf6B
+  IOracleRegistry public constant oracleRegistry = IOracleRegistry(0x2E9B7ce6124284Fb6Afe402A619278E92Ac8Cf6B);
 
   event QuoteParamsSet(address indexed baseAsset, QuoteParams quoteParams);
   event DefaultTWAPPeriodSet(uint32 twapPeriod);
